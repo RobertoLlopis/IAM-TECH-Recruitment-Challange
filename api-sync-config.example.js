c440cfe20e9229d51150e37f36a79d02181cf634
Roberto Llopis <roberto.llopis.developer@gmail.com>
2021-03-12 17:06:42 +0100

    API config connection example file

---
+++ api-sync-config.example.js
@@ -0,0 +1,9 @@
+const config = {
+  aws_access_key_id: "HERE API ID",
+  aws_appsync_graphqlEndpoint: "HERE ENDPOINT TO GRAPHQL API",
+  aws_appsync_region: "HERE REGION",
+  aws_appsync_authenticationType: "API_KEY", //* Do not touch
+  aws_appsync_apiKey: "HERE ACTUAL API KEY",
+};
+
+export default config;
